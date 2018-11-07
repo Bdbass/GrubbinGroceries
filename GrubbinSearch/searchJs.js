f43c597376e104a47a1270a5dff03eb672bb912e
Brandon Basss <bdbass@email.arizona.edu>
2018-11-07 00:17:51 -0700

    food adds to db

--- GrubbinSearch/searchJs.js
+++ GrubbinSearch/searchJs.js
@@ -11,28 +11,36 @@
 //vitamin a is in percent of dv
 //vitamin d is in percent of dv
 //vitamin k is in dv 
+require(['mongoose'], function (foo) {
+    var mongoose = foo; 
+    mongoose.connect('mongod://localhost:27017/GrubbinGroceries');
+    mongoose.Promise = global.Promise; 
+    var db = mongoose.connection;  
+    //var mongoose = require("mongoose");
+    // mongoose.connect('mongod://localhost:27017/GrubbinGroceries');
+    // mongoose.Promise = global.Promise; 
+    // var db = mongoose.connection; 
+    db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/mydb");
+    var FoodSchema = new mongoose.Schema({
+       name:      	{type: String, default: "Unknown"},
+       grams:     	{type: Number, default: 0.00 },
+       calories:  	{type: Number, default: 0.00 },
+       fat:  	  	{type: Number, default: 0.00 },
+       potassium: 	{type: Number, default: 0.00 },
+       protein:   	{type: Number, default: 0.00 },
+       sugar:     	{type: Number, default: 0.00 },
+       fiber:     	{type: Number, default: 0.00 },
+       cholesterol: {type: Number, default: 0.00 },
+       carbs: 		{type: Number, default: 0.00 },
+       vitA: 		{type: Number, default: 0.00 },
+       vitD: 		{type: Number, default: 0.00 },
+       vitK: 		{type: Number, default: 0.00 },
+    });
 
-var FoodSchema = new mongoose.Schema({
-   name:      	{type: String, default: "Unknown"},
-   grams:     	{type: Number, default: 0.00 },
-   calories:  	{type: Number, default: 0.00 },
-   fat:  	  	{type: Number, default: 0.00 },
-   potassium: 	{type: Number, default: 0.00 },
-   protein:   	{type: Number, default: 0.00 },
-   sugar:     	{type: Number, default: 0.00 },
-   fiber:     	{type: Number, default: 0.00 },
-   cholesterol: {type: Number, default: 0.00 },
-   carbs: 		{type: Number, default: 0.00 },
-   vitA: 		{type: Number, default: 0.00 },
-   vitD: 		{type: Number, default: 0.00 },
-   vitK: 		{type: Number, default: 0.00 },
+
+    var Food = mongoose.model("Food", FoodSchema);
 });
-
-
-var Food = mongoose.model("Food", FoodSchema);
 
 map = {324: "vitamin D", 318: "vitamin A", 430: "vitamin K"}; 
 
@@ -95,36 +103,46 @@ $(function(){
             "cache-control": "no-cache",
             },
             "processData": false,
-            "data": "{\n \"query\": \"" + foodObjs[i] + "\",\n \"num_servings\": 1,\n \"aggregate\": \"string\",\n \"line_delimited\": false,\n \"use_raw_foods\": false,\n \"include_subrecipe\": false,\n \"timezone\": \"US/Eastern\",\n \"consumed_at\": null,\n \"lat\": null,\n \"lng\": null,\n \"meal_type\": 0,\n \"use_branded_foods\": false,\n \"locale\": \"en_US\"\n }"
+            "data": "{\n \"query\": \"" + foodObjs[i] + "\",\n \"num_servings\": 1,\n \"aggregate\":" + 
+            + "\"string\",\n \"line_delimited\": false,\n \"use_raw_foods\": false,\n \"include_subrecipe\":" +
+            " false,\n \"timezone\": \"US/Eastern\",\n \"consumed_at\": null,\n \"lat\": null,\n \"lng\": null,\n " +
+            "\"meal_type\": 0,\n \"use_branded_foods\": false,\n \"locale\": \"en_US\"\n }"
             }
 
             await $.ajax(settings).done(function (response) {
-            	var food = new Food({
-   				   name: 		foodObjs[i],       	
-				   grams: 		response.foods[0].serving_weight_grams,
-				   calories: 	response.foods[0].nf_calories,
-				   fat: 		response.foods[0].nf_total_fat,
-				   potassium: 	response.foods[0].nf_potassium,
-				   protein: 	response.foods[0].nf_protein,
-				   sugar:     	response.foods[0].nf_sugars,
-				   fiber:     	response.foods[0].nf_dietary_fiber,
-				   cholesterol: response.foods[0].nf_cholesterol,
-				   carbs: 		response.foods[0].nf_total_carbohydrate,
-				   });
+            	if (Food.findOne({ name: foodObjs[i]})) 
+            	{
+            		$("#response").append(foodObjs[i] +" has already been added.");
+            	}
+            	else {
+            		   var food = new Food({
+	   				   name: 		foodObjs[i],       	
+					   grams: 		response.foods[0].serving_weight_grams,
+					   calories: 	response.foods[0].nf_calories,
+					   fat: 		response.foods[0].nf_total_fat,
+					   potassium: 	response.foods[0].nf_potassium,
+					   protein: 	response.foods[0].nf_protein,
+					   sugar:     	response.foods[0].nf_sugars,
+					   fiber:     	response.foods[0].nf_dietary_fiber,
+					   cholesterol: response.foods[0].nf_cholesterol,
+					   carbs: 		response.foods[0].nf_total_carbohydrate,
+					   });
 
-                // $("#response").append("name: " + foodObjs[i] + "<br />" + "grams in serving: " + response.foods[0].serving_weight_grams + "<br />" +
-                // 	"calories: " + response.foods[0].nf_calories+ "<br />" +
-                //     "fat :" + response.foods[0].nf_total_fat + "<br />" + "potassium: " + response.foods[0].nf_potassium +  "<br />" +
-                //     "protein: " + response.foods[0].nf_protein + "<br />" + "sugar: " + response.foods[0].nf_sugars +  "<br />" + 
-                //     "fiber: " + response.foods[0].nf_dietary_fiber + "<br /> cholesterol: " + response.foods[0].nf_cholesterol +  
-                //     "<br /> carbs: " + response.foods[0].nf_total_carbohydrate +  "<br />");
-                
-                temp = createMap(response.foods[0].full_nutrients); 
-                addVitamins(temp, food); 
-                
-                // $("#response").append(appendVitamins(temp) + "<br /><br />");
-
-                console.log(response.foods[0]); 
+	                // $("#response").append("name: " + foodObjs[i] + "<br />" + "grams in serving: " + response.foods[0].serving_weight_grams + "<br />" +
+	                // 	"calories: " + response.foods[0].nf_calories+ "<br />" +
+	                //     "fat :" + response.foods[0].nf_total_fat + "<br />" + "potassium: " + response.foods[0].nf_potassium +  "<br />" +
+	                //     "protein: " + response.foods[0].nf_protein + "<br />" + "sugar: " + response.foods[0].nf_sugars +  "<br />" + 
+	                //     "fiber: " + response.foods[0].nf_dietary_fiber + "<br /> cholesterol: " + response.foods[0].nf_cholesterol +  
+	                //     "<br /> carbs: " + response.foods[0].nf_total_carbohydrate +  "<br />");
+	                
+	                temp = createMap(response.foods[0].full_nutrients); 
+	                addVitamins(temp, food); 
+	                food.save(function(err, food) {
+	      				$("#response").append(food.name +" was saved.");  
+	   				});
+	                // $("#response").append(appendVitamins(temp) + "<br /><br />");
+	                console.log(response.foods[0]); 
+        		}
             }); 
         }
     });
