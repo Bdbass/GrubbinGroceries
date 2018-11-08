fe949b56fdfcca3cbc92032258b872cc65a9a911
jgatzemeier <jgatzemeier@email.arizona.edu>
2018-11-07 19:25:17 -0700

    corrected a typo in function getCollection() in Recipe.java. Added the imports for mongo in meal.java and pantry.java

--- src/main/java/gg/mealInfo/Meal.java
+++ src/main/java/gg/mealInfo/Meal.java
@@ -4,6 +4,18 @@ import java.util.ArrayList;
 import java.util.Date;
 import gg.physObjs.Food;
 
+import org.bson.Document;
+
+import com.mongodb.Block;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.DeleteResult;
+
+import static com.mongodb.client.model.Filters.*;
+
+
 public class Meal {
 	private ArrayList<Food> items; 
 	private String instructions; 
--- src/main/java/gg/physObjs/Pantry.java
+++ src/main/java/gg/physObjs/Pantry.java
@@ -2,6 +2,18 @@ package gg.physObjs;
 
 import java.util.ArrayList;
 
+import org.bson.Document;
+
+import com.mongodb.Block;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.DeleteResult;
+
+import static com.mongodb.client.model.Filters.*;
+
 public class Pantry {
 	private ArrayList<Food> food; 
+	
 }
