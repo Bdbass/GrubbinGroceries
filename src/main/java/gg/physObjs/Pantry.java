0e79a886b61915f8434c0b21c75f9e2f68074fe6
jgatzemeier <jgatzemeier@email.arizona.edu>
2018-11-07 20:00:20 -0700

    beginning on the pantry class

--- src/main/java/gg/physObjs/Pantry.java
+++ src/main/java/gg/physObjs/Pantry.java
@@ -16,4 +16,37 @@ import static com.mongodb.client.model.Filters.*;
 public class Pantry {
 	private ArrayList<Food> food; 
 	
+	public Pantry()
+	{
+		food = new ArrayList<Food>();
+	}
+	
+	public Pantry(ArrayList<Food> food)
+	{
+		this.food = food;
+	}
+	
+	//should this check if they already have some of the food and instead of adding it they just take the amount and add it to the amount of the other one
+	public void addFood(Food f1) 
+	{
+		this.food.add(f1);
+	}
+	
+	public void removeFood(Food f1)
+	{
+		for (Food f : food)
+		{
+			if (f1.getName() == f.getName())
+			{
+				if (f1.getAmount() != f.getAmount())
+				{
+					f.setAmount(f.getAmount()-f1.getAmount());
+				}
+				else
+				{
+					food.remove(f);
+				}
+			}
+		}
+	}
 }
