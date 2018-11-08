f299755234234e1623f4c441052a0218f8f035ea
jgatzemeier <jgatzemeier@email.arizona.edu>
2018-11-07 22:39:33 -0700

    Meal Plan beginning

--- src/main/java/gg/mealInfo/MealPlan.java
+++ src/main/java/gg/mealInfo/MealPlan.java
@@ -6,5 +6,21 @@ import java.util.Date;
 public class MealPlan {
 	private ArrayList<Meal> meals; 
 	private Date startDate; 
-	private int numDays; 
+	private int numDays;
+	
+	public MealPlan()
+	{
+		meals = new ArrayList<Meal>();
+		startDate = new Date();
+		numDays = 0;
+	}
+	
+	public MealPlan(Date start, int num)
+	{
+		meals = new ArrayList<Meal>();
+		startDate = start;
+		numDays = num;
+		Map<String, Double> tempPantry = new HashMap<String, Double>();
+		
+	}
 }
