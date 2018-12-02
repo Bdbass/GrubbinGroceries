74423e5f3f5cd3a06f3799575dcab2deeefaa789
jgatzemeier <jgatzemeier@email.arizona.edu>
2018-12-01 23:20:46 -0700

    started on top GUI

--- src/main/java/gg/GUI/GrubbinGUI.java
+++ src/main/java/gg/GUI/GrubbinGUI.java
@@ -2,11 +2,57 @@ package gg.GUI;
 
 import javax.swing.*;
 
-public class GrubbinGUI extends JFrame {
-
+public class GrubbinGUI extends JFrame
+{
+	JTabbedPane tabs;
+	Login login;
+	SignUp signup;
+	Homepage homepage;
+	UpdatePreferences upPref;
 	
 	
+	public GrubbinGUI()
+	{
+		super();
+		this.setTitle("Grubbin' Groceries");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		buildGUI();
+		this.add(tabs);
+		pack();
+		setVisible(true);
+	}
 	
+	public void buildGUI()
+	{
+		tabs = new JTabbedPane();
+		
+		login = new Login();
+		tabs.addTab("Log In", login);
+		
+		signup = new SignUp();
+		tabs.addTab("Sign Up", signup);
+		
+		homepage = new Homepage();
+		tabs.addTab("Home", homepage);
+		
+		upPref = new UpdatePreferences();
+		tabs.addTab("Update Restrictions", upPref);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	public static void main(String args[])
+	{
+		new GrubbinGUI();
+	}
 	
 	
 	
