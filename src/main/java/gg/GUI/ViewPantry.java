1d061b192ddb7c4714b97e4d17ac51bb356a514d
Katherine Cheetham <katherinecheetham@dhcp-10-142-179-221.uawifi.arizona.edu>
2018-12-01 17:08:03 -0700

    View pantry added

---
+++ src/main/java/gg/GUI/ViewPantry.java
@@ -0,0 +1,29 @@
+package gg.GUI;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import javax.swing.*;
+
+import gg.physObjs.*;	//Pantry class
+import gg.userInfo.*;	//Person class
+
+public class ViewPantry extends JPanel
+{
+	
+	public ViewPantry() {
+		buildViewPantry();
+	}
+	
+	private void buildViewPantry() {
+		//labels for the top
+		//scrolling window
+		//add food and delete food buttons
+	}
+	
+	public static void main(String agrs[]) {
+		
+		new ViewPantry();
+	}
+}
